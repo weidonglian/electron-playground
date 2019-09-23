63049206687b17e4afe6c15bb13bd803299e0faa
Weidong Lian <weidong.lian@gmail.com>
2019-09-23 23:03:17 +0200

    electron app using webpack typescript react

---
+++ electron-app/src/react.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+const Index = () => {
+    return (
+        <div>Hello React!</div>
+    )
+}
+
+ReactDOM.render(<Index />, document.getElementById('app'))
